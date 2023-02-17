ffd52f4bb7736a525c215a9326a424593aab6788
Darth Vader <danieltarancon@gmail.com>
2023-02-17 17:26:14 +0100

    Update prk.py

--- prk.py
+++ prk.py
@@ -1,19 +1,12 @@
-intlist = []
-total = []
-program = True
-while program:
-    choice = int(input('Enter a element of the list please'))
-    intlist.append(choice)
-    cont = input('Enter "N" if you are done with your list. Otherwise press any other key to continue').lower()
-    if cont == 'n':
-        program = False
-for i in intlist:
-    if (i % 2) == 0:
-        total.append(i)
-    else:
-        continue
+import math
 
-print(sum(total), intlist)
+number = math.gcd(23)
+
+num = math.gcd(4)
+
+print(num, number)
+
+
 
 
 
